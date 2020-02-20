2df25aae56cabcbf70f332969d181ecbe1b52f89
GuillaumeBenoot <guillaume.benoot@gmail.com>
2020-02-20 11:45:47 +0100

    acceuil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableauAccueilComponent } from './tableau-accueil/tableau-accueil.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: TableauAccueilComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
